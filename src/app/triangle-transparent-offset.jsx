3868e183bad65ffdbdd0d39ecc7a5fefad019c80
Jesse Cooper <J-L-C@users.noreply.github.com>
2024-02-09 12:26:37 -0600

    Complete under construction site

---
+++ src/app/triangle-transparent-offset.jsx
@@ -0,0 +1,14 @@
+export default function TriangleTransparentOffset({className}) {
+    return (
+        <>
+            <svg className={className} viewBox="0 0 62 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path
+                    d="M8.2998 73.2001V11.6001L61.5998 42.4001L8.2998 73.2001ZM11.3998 17.1001V67.8001L55.2998 42.4001L11.3998 17.1001Z"
+                    fill="#9D9D9D"/>
+                <path d="M0 61.5V0L53.3 30.8L0 61.5ZM3.09999 5.29999V56L47 30.6L3.09999 5.29999Z" fill="currentColor"/>
+                <path d="M0 61.5V0L53.3 30.8L0 61.5ZM3.09999 5.29999V56L47 30.6L3.09999 5.29999Z" fill="currentColor"
+                      fill-opacity="0.2"/>
+            </svg>
+        </>
+    )
+}
